19336dbde0a96ed3071428766bfcf4ab9e9f9be2
AlexS778 <aleksej.serbinov29@gmail.com>
2023-12-03 14:58:28 +0300

    feat: better comments

--- 1/main.py
+++ 1/main.py
@@ -1,22 +1,32 @@
 import pygame
 import sys
 
+# инициализуруем модуль pygame
 pygame.init()
 
+# создаем экран размером 400 x 300
 screen = pygame.display.set_mode((400, 300))
+# создаем название окошка экрана
 pygame.display.set_caption("Пример ввода/вывода")
 
-name = input("Enter your name: ")
+# запрашиваем имя пользователя в терминале
+name = input("Введите ваше имя: ")
 
-font = pygame.font.Font(None, 36)
+# создаем шрифт с размером 50
+font = pygame.font.Font(None, 50)
+# создаем надпись
 text = font.render(f"Привет, {name}!", True, (255, 255, 255))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            print("пользователь нажал на закрытие")
             pygame.quit()
             sys.exit()
 
+    # заполянем экран с помощью функции fill
     screen.fill((0, 0, 0))
+    # рисуем наш тест на экране с помощью blit
     screen.blit(text, (50, 50))
+    # создаем экран
     pygame.display.flip()
--- 2/main.py
+++ 2/main.py
@@ -4,14 +4,13 @@ import sys
 pygame.init()
 
 screen = pygame.display.set_mode((400, 300))
-pygame.display.set_caption("Conditional Operator Example")
+pygame.display.set_caption("Пример if/else")
 
-condition = False
-if condition is True:
-    color = (0, 255, 0)
+color = True
+if color is True:
+    color = (43, 255, 0)
 else:
     color = (255, 0, 0)
-color = (0, 255, 0) if condition else (255, 0, 0)
 
 while True:
     for event in pygame.event.get():
@@ -19,6 +18,11 @@ while True:
             pygame.quit()
             sys.exit()
 
+    # заполнить экран черным
     screen.fill((0, 0, 0))
+    # pygame.draw.rect - нарисовать квардрат на экране 
+    # 1 аргументом в функцию передаем экран, на котором будем рисовать
+    # 2 аргументом в функцию передаем цвет рисунка
+    # 3 аргументом передаем размер рисунка
     pygame.draw.rect(screen, color, (50, 50, 100, 100))
     pygame.display.flip()
