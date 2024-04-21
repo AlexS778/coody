c1ab6afb2cfb142857f8c132a657c70479b73113
AlexS778 <aleksej.serbinov29@gmail.com>
2024-04-21 11:25:23 +0300

    feat: player

--- 0.Project game/game.py
+++ 0.Project game/game.py
@@ -1,7 +1,5 @@
 import pygame
 
-pygame.init()
-
 from pygame.locals import (
     RLEACCEL,
     K_UP,
@@ -13,24 +11,24 @@ from pygame.locals import (
     QUIT,
 )
 
-
-screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("любое название")
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        player_image = pygame.image.load("./jet.png").convert()
+        player_image = pygame.image.load(
+            "сюда подставьте название вашей картинки"
+        ).convert()
         self.surf = pygame.transform.scale(player_image, (62, 25))
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.rect = self.surf.get_rect()
         self.rect.x = 0
-        self.rect.y = 500 // 2
-        # ваш код, здоровье и тд
+        self.rect.y = SCREEN_HEIGHT // 2
         self.score = 0
 
-    # Move the sprite based on keypresses
+    # двигаем спрайт
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -5)
@@ -41,19 +39,54 @@ class Player(pygame.sprite.Sprite):
         if pressed_keys[K_RIGHT]:
             self.rect.move_ip(5, 0)
 
-        # Keep player on the screen
+        # проверяем не вышел ли он за границы
         if self.rect.left < 0:
             self.rect.left = 0
-        elif self.rect.right > 500:
-            self.rect.right = 500
+        elif self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
         if self.rect.top <= 0:
             self.rect.top = 0
-        elif self.rect.bottom >= 500:
-            self.rect.bottom = 500
+        elif self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
 
 
-while True:
+pygame.init()
+
+# часы для фпс
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# создаем игрока
+player = Player()
+
+# создаем группу спрайтов
+all_sprites = pygame.sprite.Group()
+# в эту группу спрайтов добавляем игрока
+all_sprites.add(player)
+
+running = True
+
+while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    screen.fill((135, 206, 250))
+
+    # рисуем все спрайты из группы
+    for entity in all_sprites:
+        screen.blit(entity.surf, entity.rect)
+
+    # обновялем экран со всеми нарисованными спрайтами
     pygame.display.flip()
+
+    # делаем фпс 30
+    clock.tick(30)
